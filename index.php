2170511ad479e1d1652879fdb1f58da5309830f5
Ezequiel <73133884+ezecastrog@users.noreply.github.com>
2024-10-21 18:54:04 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+ <head>
+  <title>
+    Login UNSO
+  </title>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"/>
+ </head>
+ <body>
+  <div class="container">
+   <div class="left">
+    <h1>
+     UNIVERSIDAD NACIONAL RAÚL SCALABRINI ORTIZ SAN ISIDRO
+    </h1>
+
+   </div>
+   <div class="right">
+
+    <div class="form-group">
+      <h2>Usuario</h2>
+     <input placeholder="Email Address" type="email"/ id="email">
+    </div>
+    <div class="form-group">
+      <h2><br>Contraseña</h2>
+     <input placeholder="Password" type="password" id="password"/>
+    </div>
+    <button class="btn">
+     Ingresar
+    </button>
+    <p class="register">
+        <a href="">Registrarse?</a>
+    </p>
+    <p class="forgot-password">
+        <a href="">Recuperar contraseña?</a>
+    </p>
+   </div>
+  </div>
+ </body>
+</html>
